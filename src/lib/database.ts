aafbfcd40b271b7feebb92d3fc5e7bd904982b01
adegbenro marvellous <alinco2319@gmail.com>
2024-07-25 00:10:50 +0100

    new update

--- src/lib/database.ts
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-// mongoose.connect()
-const connectionString : string = 
-mongoose.connect(connectionString, { useNewUrlParser: true, useUnifiedTopology: true })
-     .then(() => {
-          console.log('Connected to Database');
-     })
-     .catch(err => console.error(err));
